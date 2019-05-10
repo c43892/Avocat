59bd38fa7a18934fe4a9b98f583e940ef326294b
Deanxx51 <35351330+Deanxx51@users.noreply.github.com>
2019-05-10 11:20:14 -0400

    新建包含Setting的UI文件夹

---
+++ Client/Assets/Scripts/Stages/StageUI/SettingUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingUI : MonoBehaviour
+{
+    public void OnClickSetting() {
+        Debug.Log("This is Setting!");
+    }
+}
