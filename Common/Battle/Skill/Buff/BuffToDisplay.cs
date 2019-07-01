3fbcae15cc2cb67eaa18edc4cd580c8d6af89fe5
Deanxx51 <35351330+Deanxx51@users.noreply.github.com>
2019-07-01 15:20:13 -0400

    修改了buff显示列表逻辑位置

--- Common/Battle/Skill/Buff/BuffToDisplay.cs
+++ Common/Battle/Skill/Buff/BuffToDisplay.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuffToDisplay : MonoBehaviour
+public class BuffToDisplay
 {
    public string[] BuffsToDisplay { get; set; }
 }
